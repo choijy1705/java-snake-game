ce1bc448ca0844938329ba3e1446e4c7ca8ff88a
Junyoung Choi <cjy1705@naver.com>
2020-12-01 10:57:23 +0900

    feat: 화면 구성 색 설정 구현

--- src/Window.java
+++ src/Window.java
@@ -1,12 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Window extends JFrame {
     private static final long serialVersionUID = -2542001418764869760L;
     public static ArrayList<ArrayList<DataOfSquare>> Grid;
+    public static int width = 20;
+    public static int height = 20;
 
     public Window(){
-        Grid = new ArrayList<ArrayList<DataOfSquare>>();
+        Grid = new ArrayList<>();
+        ArrayList<DataOfSquare> data;
+
+        for(int i=0;i<width;i++){
+            data = new ArrayList<DataOfSquare>();
+            for(int j=0;j<height;j++){
+                DataOfSquare c = new DataOfSquare(2);
+                data.add(c);
+            }
+            Grid.add(data);
+
+        }
+
+        getContentPane().setLayout(new GridLayout(20,20,0,0));
+
+        for(int i=0;i<width;i++){
+            for(int j=0;j<height;j++){
+                getContentPane().add(Grid.get(i).get(j).square);
+            }
+        }
+
+
     }
 
 }
