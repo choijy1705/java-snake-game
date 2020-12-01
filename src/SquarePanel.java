70d9fb7967f86fddfdbb7f5ed5c9095009d7eba8
Junyoung Choi <cjy1705@naver.com>
2020-12-01 10:48:01 +0900

    feat: SquarePanel 생성

---
+++ src/SquarePanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SquarePanel extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    public SquarePanel(Color d){
+        this.setBackground(d);
+    }
+}
